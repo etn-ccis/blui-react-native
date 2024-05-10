cbe69ec93c9a6202715190336d61871538ab70bb
manojleaton <manojlokesh@eaton.com>
2024-05-10 17:02:35 +0530

    removed WebVitals

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -23,7 +23,6 @@ import '@fontsource/roboto';
 import '@fontsource/roboto-mono';
 import {App} from './app/app';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import {store, RootState} from './redux/store';
 import {useAppSelector} from './redux/hooks';
 import {MDXProvider} from '@mdx-js/react';
@@ -92,8 +91,3 @@ root.render(
     </BrowserRouter>
   </StyledEngineProvider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
