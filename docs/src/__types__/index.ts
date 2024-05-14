b6c3d77ea6106ceb27c242a52ebe57d910161a2f
manojleaton <manojlokesh@eaton.com>
2024-05-14 18:35:13 +0530

    updated packages list

--- docs/src/__types__/index.ts
+++ docs/src/__types__/index.ts
@@ -1,15 +1,17 @@
 export type RangeDataTypes = {
-    min: number;
-    max: number;
-    step: number;
+  min: number;
+  max: number;
+  step: number;
 };
+
 export type SiteThemeType = 'light' | 'dark' | 'system';
+
 export type UIDirection = 'ltr' | 'rtl';
 
 export type SiteThemePayloadType = {
-    siteTheme: SiteThemeType;
+  siteTheme: SiteThemeType;
 };
 
 export type SiteDirectionPayloadType = {
-    siteDirection: UIDirection;
+  siteDirection: UIDirection;
 };
