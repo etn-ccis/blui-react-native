7e58866b083bfb46d79f5c2b01e03475abba3a00
manojleaton <manojlokesh@eaton.com>
2024-05-23 20:15:00 +0530

    github actions config

--- docs/src/app/app.tsx
+++ docs/src/app/app.tsx
@@ -4,7 +4,6 @@ import {NavigationDrawer} from '../router/drawer';
 import {MainRouter} from '../router/main';
 import {DrawerLayout} from '@brightlayer-ui/react-components';
 import {Routes} from 'react-router-dom';
-import Test from './test.mdx';
 
 export const App = (): JSX.Element => {
   const [drawerOpen, setDrawerOpen] = useState(false);
