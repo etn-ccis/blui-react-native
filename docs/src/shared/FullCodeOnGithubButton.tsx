bdca8aabb412b39b2cd8a9bd21857623177fa3fa
Arshdeep Singh <arshdeepsingh@HMECMB00375.local>
2024-05-28 18:52:21 +0530

    update github links

--- docs/src/shared/FullCodeOnGithubButton.tsx
+++ docs/src/shared/FullCodeOnGithubButton.tsx
@@ -13,7 +13,7 @@ export const FullCodeOnGithub: React.FC<FullCodeOnGithubProps> = props => (
   <Button
     variant="outlined"
     target="_blank"
-    href={`https://github.com/etn-ccis/blui-react-component-library/blob/${DOCS_BRANCH}/docs/src/${props.url}`}
+    href={`https://github.com/etn-ccis/blui-react-native/blob/${DOCS_BRANCH}/docs/src/${props.url}`}
     startIcon={<GitHubIcon />}
     sx={props.sx}>
     Full Code on GitHub
--- docs/src/shared/markdown/ForMoreDetailsOnStylingOptions.tsx
+++ docs/src/shared/markdown/ForMoreDetailsOnStylingOptions.tsx
@@ -11,7 +11,7 @@ export const ForMoreDetailsOnStylingOptions = (): JSX.Element => {
     <Box sx={{color: 'text.primary', mb: 2}}>
       For more details on styling options check out our{' '}
       <a
-        href={`https://github.com/etn-ccis/blui-react-component-library/blob/${DOCS_BRANCH}/docs/src/shared/markdown/StyleOverridesGuide.md`}
+        href={`https://github.com/etn-ccis/blui-react-native/blob/${DOCS_BRANCH}/docs/src/shared/markdown/StyleOverridesGuide.md`}
         style={{color: theme.palette.primary.main}}>
         Styling Guide
       </a>
