a6d51b2ae048bd4be8c1c7ab2e40413389a53cad
priyankakmEaton <priyankakm@eaton.com>
2024-06-11 20:49:58 +0530

    convert  spacer example

--- docs/src/componentDocs/Spacer/examples/SpacerWithPixel.tsx
+++ docs/src/componentDocs/Spacer/examples/SpacerWithPixel.tsx
@@ -5,24 +5,24 @@ import {SpacerWithPixelExample} from './SpacerWithPixelExample';
 
 const codeSnippet = `
 <View style={{display: 'flex', flexDirection: 'row', height: 56}}>
-    <Spacer width={25} flex={0} style={{backgroundColor: '#4682B4'}}>
+    <Spacer width={25} flex={0} style={{backgroundColor: '#4da3d4' }}>
         <Text>25</Text>
     </Spacer>
-    <Spacer width={75} flex={0} style={{backgroundColor: '#FFD700'}}>
+    <Spacer width={75} flex={0} style={{backgroundColor: '#f5db6d'}}>
         <Text>75</Text>
     </Spacer>
-    <Spacer width={200} flex={0} style={{backgroundColor: '#FF6347'}}>
+    <Spacer width={200} flex={0} style={{backgroundColor: '#da7777'}}>
         <Text>200</Text>
     </Spacer>
 </View>
 <View style={{display: 'flex', flexDirection: 'column', width: 300}}>
-    <Spacer height={25} flex={0} style={{backgroundColor: '#4682B4'}}>
+    <Spacer height={25} flex={0} style={{backgroundColor: '#4da3d4'}}>
         <Text>25</Text>
     </Spacer>
-    <Spacer height={50} flex={0} style={{backgroundColor: '#FFD700'}}>
+    <Spacer height={50} flex={0} style={{backgroundColor: '#f5db6d'}}>
         <Text>50</Text>
     </Spacer>
-    <Spacer height={75} flex={0} style={{backgroundColor: '#FF6347'}}>
+    <Spacer height={75} flex={0} style={{backgroundColor: '#da7777'}}>
         <Text>75</Text>
     </Spacer>
 </View>
