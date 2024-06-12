c236be5a6eb19297bc07ea50b6e245c518f7f0e5
shubham-eaton <ShubhamKVishwakarma@eaton.com>
2024-06-12 19:31:09 +0530

    Converted Overline into live code

---
+++ docs/src/componentDocs/Overline/examples/OverlineExample.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Overline} from '@brightlayer-ui/react-native-components';
+import {ExampleShowcase} from '../../../shared';
+
+export const OverlineExample = (): JSX.Element => (
+  <ExampleShowcase>
+    <Overline>Overline</Overline>
+  </ExampleShowcase>
+);
