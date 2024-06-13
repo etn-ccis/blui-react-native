e93d8b776e33d2ab92635657e5aefb413178b0c3
komal-eaton <KomalRanjit@eaton.com>
2024-06-13 13:53:36 +0530

    Added package for MatIcon

--- docs/src/componentDocs/Icons/examples/IconAsProp.tsx
+++ docs/src/componentDocs/Icons/examples/IconAsProp.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
 import {IconAsPropExample} from './IconAsPropExample';
 
-const codeSnippet = `<Hero label={'Setting'} icon={{family:'material', name:'settings'}} />`;
+const codeSnippet = `<Hero label={'Setting'} icon={<MatIcon name="settings" size={38} color={'green'} />} />`;
 
 export const IconAsProp = (): JSX.Element => (
   <Box>
