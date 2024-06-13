12c8ab067bf40bb376693935801fbf534e438940
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2024-06-13 09:39:50 -0400

    Add header examples

---
+++ docs/src/componentDocs/Header/examples/HeaderExample.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ExampleShowcase } from '../../../shared';
+import { Header } from '@brightlayer-ui/react-native-components';
+
+export const HeaderExample = (): JSX.Element => (
+    <ExampleShowcase>
+        <Header
+            title={'Valley Forge'}
+            subtitle={'The Last Stand'}
+        />
+    </ExampleShowcase>
+);
