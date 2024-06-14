cf8629a34a51e8cce83da77a46682c6e6e0bec0b
manojleaton <manojlokesh@eaton.com>
2024-06-14 16:49:53 +0530

    chip component live code example docs

--- docs/src/componentDocs/Chip/examples/ChipSelect.tsx
+++ docs/src/componentDocs/Chip/examples/ChipSelect.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {Box} from '@mui/material';
-import {ChipExample} from './ChipExample';
 import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
+import {ChipSelectExample} from './ChipSelectExample';
 
-const codeSnippet = `<Chip>Outlined Chip</Chip>
-<Chip mode="elevated">Elevated Chip</Chip>`;
+const codeSnippet = `<Chip>Unselected Chip</Chip>
+<Chip selected>Selected Chip</Chip>`;
 
-export const Chip = (): JSX.Element => (
+export const ChipSelect = (): JSX.Element => (
   <Box>
-    <ChipExample />
+    <ChipSelectExample />
     <CodeBlock code={codeSnippet} language={'jsx'} />
     <CodeBlockActionButtonRow
       copyText={codeSnippet}
-      url="componentDocs/Chip/examples/Chip.tsx"
+      url="componentDocs/Chip/examples/ChipSelect.tsx"
     />
   </Box>
 );
--- docs/src/componentDocs/Chip/examples/ChipSelectExample.tsx
+++ docs/src/componentDocs/Chip/examples/ChipSelectExample.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {Chip} from '@brightlayer-ui/react-native-components';
 import {ExampleShowcase} from '../../../shared';
 
-export const ChipExample = (): JSX.Element => (
+export const ChipSelectExample = (): JSX.Element => (
   <ExampleShowcase sx={{display: 'flex'}}>
-    <Chip style={{marginRight: 10}}>Outlined Chip</Chip>
-    <Chip mode="elevated">Elevated Chip</Chip>
+    <Chip style={{marginRight: 10}}>Unselected Chip</Chip>
+    <Chip selected>Selected Chip</Chip>
   </ExampleShowcase>
 );
