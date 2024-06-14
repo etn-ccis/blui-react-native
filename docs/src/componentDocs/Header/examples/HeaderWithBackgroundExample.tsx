877ed2385f7b3b260e4d480a3c1b34a66babfcb6
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2024-06-14 09:15:46 -0400

    image

--- docs/src/componentDocs/Header/examples/HeaderWithBackgroundExample.tsx
+++ docs/src/componentDocs/Header/examples/HeaderWithBackgroundExample.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {ExampleShowcase} from '../../../shared';
 import {Header} from '@brightlayer-ui/react-native-components';
+// import BackgroundImage from '../../../assets/farm.jpg';
 
 export const HeaderWithBackgroundExample = (): JSX.Element => (
   <ExampleShowcase>
     <Header
       title={'Valley Forge'}
       subtitle={'The Last Stand'}
-      // backgroundImage={require('../assets/images/farm.jpg')}
+      // backgroundImage={BackgroundImage}
+      // backgroundImage={require('../../../assets/farm.jpg')}
     />
   </ExampleShowcase>
 );
