aa3f8013894ede2491a4bae83c2b7a736bf6d317
priyankakmEaton <priyankakm@eaton.com>
2024-06-16 23:20:01 +0530

    convert Example docs

--- docs/src/componentDocs/ListItemTag/examples/ListItemTagUsingInfoListItem.tsx
+++ docs/src/componentDocs/ListItemTag/examples/ListItemTagUsingInfoListItem.tsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import { CodeBlock, CodeBlockActionButtonRow } from '../../../shared';
-import { ListItemTagUsingInfoListItemExample } from './ListItemTagUsingInfoListItemExample';
+import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
+import {ListItemTagUsingInfoListItemExample} from './ListItemTagUsingInfoListItemExample';
 
-const codeSnippet = `<InfoListItem
-    icon={<BatteryChargingFull />}
+const codeSnippet = `
+  <InfoListItem
+    icon={{name: 'battery-charging-full'}}
+    iconColor="#528AAE"
+    style={{backgroundColor: Colors.white[100]}}
     title="Info List Item"
     subtitle="with a ListItemTag component to the right"
-    iconColor="text.primary"
     rightComponent={
-        <Stack spacing={2} direction={'row'}>
-            <ListItemTag
-                label="Build Passing"
-                backgroundColor={Colors.green[300]}
-                fontColor={Colors.black[900]}
-            />
-            <ListItemTag label="5 Bugs" backgroundColor={Colors.red[300]} fontColor={Colors.black[900]} />
-        </Stack>
+      <View style={{flexDirection: 'row'}}>
+        <ListItemTag
+          label="Build Passing"
+          backgroundColor={Colors.green[300]}
+          fontColor={Colors.black[900]}
+        />
+        <View style={{width: 15}} />
+        <ListItemTag
+          label="5 Bugs"
+          backgroundColor={Colors.red[300]}
+          fontColor={Colors.black[900]}
+        />
+      </View>
     }
-/>`;
+  />
+  `;
 
 export const ListItemTagUsingInfoListItem = (): JSX.Element => (
-    <Box>
-        <ListItemTagUsingInfoListItemExample />
-        <CodeBlock code={codeSnippet} language="jsx" dataLine="9-14" />
-        <CodeBlockActionButtonRow
-            copyText={codeSnippet}
-            url="componentDocs/ListItemTag/examples/ListItemTagUsingInfoListItemExample.tsx"
-        />
-    </Box>
+  <Box>
+    <ListItemTagUsingInfoListItemExample />
+    <CodeBlock code={codeSnippet} language="jsx" dataLine="9-14" />
+    <CodeBlockActionButtonRow
+      copyText={codeSnippet}
+      url="componentDocs/ListItemTag/examples/ListItemTagUsingInfoListItemExample.tsx"
+    />
+  </Box>
 );
--- docs/src/componentDocs/ListItemTag/examples/ListItemTagUsingInfoListItemExample.tsx
+++ docs/src/componentDocs/ListItemTag/examples/ListItemTagUsingInfoListItemExample.tsx
@@ -6,13 +6,15 @@ import {
 import * as Colors from '@brightlayer-ui/colors';
 import {ExampleShowcase} from '../../../shared';
 import {View} from 'react-native';
+
 export const ListItemTagUsingInfoListItemExample = (): JSX.Element => (
   <ExampleShowcase sx={{display: 'flex', justifyContent: 'center'}}>
     <InfoListItem
       icon={{name: 'battery-charging-full'}}
+      iconColor="#528AAE"
+      style={{backgroundColor: Colors.white[100]}}
       title="Info List Item"
       subtitle="with a ListItemTag component to the right"
-      iconColor="text.primary"
       rightComponent={
         <View style={{flexDirection: 'row'}}>
           <ListItemTag
@@ -20,6 +22,7 @@ export const ListItemTagUsingInfoListItemExample = (): JSX.Element => (
             backgroundColor={Colors.green[300]}
             fontColor={Colors.black[900]}
           />
+          <View style={{width: 15}} />
           <ListItemTag
             label="5 Bugs"
             backgroundColor={Colors.red[300]}
