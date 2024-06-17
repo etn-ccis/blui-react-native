2cbc3ac0be79dd117456be86075938e7d73615a9
komal-eaton <KomalRanjit@eaton.com>
2024-06-17 17:18:04 +0530

    Aligned grades in rows

--- docs/src/componentDocs/Grade/examples/GradeExample.tsx
+++ docs/src/componentDocs/Grade/examples/GradeExample.tsx
@@ -3,19 +3,30 @@ import {ExampleShowcase} from '../../../shared';
 import {Grade} from '@brightlayer-ui/react-native-components';
 
 export const GradeExample = (): JSX.Element => (
-  <ExampleShowcase sx={{display: 'flex', justifyContent: 'center'}}>
-    <Grade.APlus />
-    <Grade.A />
-    <Grade.AMinus />
-    <Grade.BPlus />
-    <Grade.B />
-    <Grade.BMinus />
-    <Grade.CPlus />
-    <Grade.C />
-    <Grade.CMinus />
-    <Grade.DPlus />
-    <Grade.D />
-    <Grade.DMinus />
-    <Grade.F />
+  <ExampleShowcase
+    sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+      <Grade.APlus />
+      <Grade.A />
+      <Grade.AMinus />
+    </div>
+    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+      <Grade.BPlus />
+      <Grade.B />
+      <Grade.BMinus />
+    </div>
+    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+      <Grade.CPlus />
+      <Grade.C />
+      <Grade.CMinus />
+    </div>
+    <div style={{display: 'flex', justifyContent: 'space-around'}}>
+      <Grade.DPlus />
+      <Grade.D />
+      <Grade.DMinus />
+    </div>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <Grade.F />
+    </div>
   </ExampleShowcase>
 );
