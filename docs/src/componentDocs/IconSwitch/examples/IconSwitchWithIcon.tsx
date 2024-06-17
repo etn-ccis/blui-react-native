b9731774b3baac175718fd000844e9a140c5819c
priyankakmEaton <priyankakm@eaton.com>
2024-06-17 14:13:28 +0530

    convert Iconswitch Example

---
+++ docs/src/componentDocs/IconSwitch/examples/IconSwitchWithIcon.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
+import {IconSwitchWithIconExample} from './IconSwitchWithIconExample';
+
+const codeSnippet = `
+  <View style={{flexDirection: 'column'}}>
+    <View style={{padding: 10}}>
+      <IconSwitch value={false} showIcon />
+    </View>
+    <View style={{padding: 10}}>
+      <IconSwitch value={true} showIcon />
+    </View>
+    <View style={{padding: 10}}>
+      <IconSwitch disabled showIcon />
+    </View>
+    <View style={{padding: 10}}>
+      <IconSwitch value disabled showIcon />
+    </View>
+  </View>
+  `;
+export const IconSwitchWithIcon = (): JSX.Element => (
+  <Box>
+    <IconSwitchWithIconExample />
+    <CodeBlock code={codeSnippet} language="jsx" />
+    <CodeBlockActionButtonRow
+      copyText={codeSnippet}
+      url="componentDocs/IconSwitch/examples/IconSwitchWithIconExample.tsx"
+    />
+  </Box>
+);
