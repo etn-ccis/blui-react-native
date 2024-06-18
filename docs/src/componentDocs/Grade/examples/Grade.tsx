e0719f222623b4e1f62183a70197dd5d14dc086d
komal-eaton <KomalRanjit@eaton.com>
2024-06-18 15:45:38 +0530

    Formatted

--- docs/src/componentDocs/Grade/examples/Grade.tsx
+++ docs/src/componentDocs/Grade/examples/Grade.tsx
@@ -3,55 +3,19 @@ import Box from '@mui/material/Box';
 import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
 import {GradeExample} from './GradeExample';
 
-const codeSnippet = `<div style={{display: 'flex', justifyContent: 'space-around'}}>
-      <div style={{margin: '10px'}}>
-        <Grade.APlus />
-      </div>
-      <div style={{margin: '10px'}}>
-        <Grade.A />
-      </div>
-      <div style={{margin: '10px'}}>
-        <Grade.AMinus />
-      </div>
-    </div>
-    <div style={{display: 'flex', justifyContent: 'space-around'}}>
-      <div style={{margin: '10px'}}>
-        <Grade.BPlus />
-      </div>
-      <div style={{margin: '10px'}}>
-        <Grade.B />
-      </div>
-      <div style={{margin: '10px'}}>
-        <Grade.BMinus />
-      </div>
-    </div>
-    <div style={{display: 'flex', justifyContent: 'space-around'}}>
-      <div style={{margin: '10px'}}>
-        <Grade.CPlus />
-      </div>
-      <div style={{margin: '10px'}}>
-        <Grade.C />
-      </div>
-      <div style={{margin: '10px'}}>
-        <Grade.CMinus />
-      </div>
-    </div>
-    <div style={{display: 'flex', justifyContent: 'space-around'}}>
-      <div style={{margin: '10px'}}>
-        <Grade.DPlus />
-      </div>
-      <div style={{margin: '10px'}}>
-        <Grade.D />
-      </div>
-      <div style={{margin: '10px'}}>
-        <Grade.DMinus />
-      </div>
-    </div>
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <div style={{margin: '10px'}}>
-        <Grade.F />
-      </div>
-    </div>`;
+const codeSnippet = `<Grade.APlus/>
+<Grade.A/>
+<Grade.AMinus/>
+<Grade.BPlus/>
+<Grade.B/>
+<Grade.BMinus/>
+<Grade.CPlus/>
+<Grade.C/>
+<Grade.CMinus/>
+<Grade.DPlus/>
+<Grade.D/>
+<Grade.DMinus/>
+<Grade.F/>`;
 
 export const Grade = (): JSX.Element => (
   <Box>
