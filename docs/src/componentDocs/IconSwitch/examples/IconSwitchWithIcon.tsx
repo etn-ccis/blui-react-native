329362bdcc4b0a252513be5b3eee20abce2de222
priyankakmEaton <priyankakm@eaton.com>
2024-06-18 22:24:28 +0530

    Adding Dataline

--- docs/src/componentDocs/IconSwitch/examples/IconSwitchWithIcon.tsx
+++ docs/src/componentDocs/IconSwitch/examples/IconSwitchWithIcon.tsx
@@ -3,26 +3,24 @@ import Box from '@mui/material/Box';
 import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
 import {IconSwitchWithIconExample} from './IconSwitchWithIconExample';
 
-const codeSnippet = `
-  <View style={{flexDirection: 'column'}}>
-    <View style={{padding: 10}}>
-      <IconSwitch value={false} showIcon />
-    </View>
-    <View style={{padding: 10}}>
-      <IconSwitch value={true} showIcon />
-    </View>
-    <View style={{padding: 10}}>
-      <IconSwitch disabled showIcon />
-    </View>
-    <View style={{padding: 10}}>
-      <IconSwitch value disabled showIcon />
-    </View>
+const codeSnippet = `<View style={{flexDirection: 'column'}}>
+  <View style={{padding: 10}}>
+    <IconSwitch value={false} showIcon />
   </View>
-  `;
+  <View style={{padding: 10}}>
+    <IconSwitch value={true} showIcon />
+  </View>
+  <View style={{padding: 10}}>
+    <IconSwitch disabled showIcon />
+  </View>
+  <View style={{padding: 10}}>
+    <IconSwitch value disabled showIcon />
+  </View>
+</View>`;
 export const IconSwitchWithIcon = (): JSX.Element => (
   <Box>
     <IconSwitchWithIconExample />
-    <CodeBlock code={codeSnippet} language="jsx" />
+    <CodeBlock code={codeSnippet} language="jsx" dataLine="3,6,9,12" />
     <CodeBlockActionButtonRow
       copyText={codeSnippet}
       url="componentDocs/IconSwitch/examples/IconSwitchWithIconExample.tsx"
