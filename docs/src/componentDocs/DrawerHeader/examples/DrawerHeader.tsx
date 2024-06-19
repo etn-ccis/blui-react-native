22f46c3b936f62a810d49cab86d00ba52fa1b93c
priyankakmEaton <priyankakm@eaton.com>
2024-06-19 17:48:44 +0530

    add example docs

---
+++ docs/src/componentDocs/DrawerHeader/examples/DrawerHeader.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
+import {DrawerHeaderExample} from './DrawerHeaderExample';
+
+const codeSnippet = `<Drawer  width={250}>
+    <DrawerHeader title="Title" subtitle="Subtitle" icon={<Menu />} />
+</Drawer>
+`;
+
+export const DrawerHeader = (): JSX.Element => (
+  <Box>
+    <DrawerHeaderExample />
+    <CodeBlock code={codeSnippet} language="jsx" dataLine="2" />
+    <CodeBlockActionButtonRow
+      copyText={codeSnippet}
+      url="componentDocs/DrawerHeader/examples/DrawerHeaderExample.tsx"
+    />
+  </Box>
+);
