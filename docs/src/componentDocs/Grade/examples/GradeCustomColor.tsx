e3ea061dda4cdd573f06438035083f13fee46c6e
komal-eaton <KomalRanjit@eaton.com>
2024-06-19 17:18:55 +0530

    PR comments resolved

--- docs/src/componentDocs/Grade/examples/GradeCustomColor.tsx
+++ docs/src/componentDocs/Grade/examples/GradeCustomColor.tsx
@@ -3,12 +3,16 @@ import Box from '@mui/material/Box';
 import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
 import {GradeCustomColorExample} from './GradeCustomColorExample';
 
-const codeSnippet = `<Grade label='AB' fontColor='#ff0' backgroundColor='#f00'/>`;
+const codeSnippet = `<Grade 
+      label="AB"
+      fontColor="#ff0"
+      backgroundColor="#f00"
+    />`;
 
 export const GradeCustomColor = (): JSX.Element => (
   <Box>
     <GradeCustomColorExample />
-    <CodeBlock code={codeSnippet} language="jsx" />
+    <CodeBlock code={codeSnippet} language="jsx" dataLine="3-4" />
     <CodeBlockActionButtonRow
       copyText={codeSnippet}
       url="componentDocs/Grade/examples/GradeCustomColourExample.tsx"
