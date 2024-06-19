2640c55f6a608cae7790644a73468dffc54f5873
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2024-06-19 09:16:29 -0400

    update snippet

--- docs/src/componentDocs/ScoreCard/examples/ScoreCardWithBadge.tsx
+++ docs/src/componentDocs/ScoreCard/examples/ScoreCardWithBadge.tsx
@@ -4,34 +4,41 @@ import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
 import {ScoreCardWithBadgeExample} from './ScoreCardWithBadgeExample';
 
 const codeSnippet = `<ScoreCard
-    headerTitle="Station 3"
-    headerSubtitle="High Humidity Alarm"
-    headerInfo="4 Devices"
-    badge={
-        <HeroBanner
-            <Hero
-                icon={{ family: 'brightlayer-ui', name: 'temp' }}
-                label="Temperature"
-                ChannelValueProps={{ value: 98, units: '°F' }}
-                style={{ overflow: 'visible' }}
-            />
-            <Hero
-                icon={{ family: 'brightlayer-ui', name: 'moisture' }}
-                label="Humidity"
-                ChannelValueProps={{ value: 54, units: '%' }}
-            />
-        </HeroBanner>
-    }
-    >
-    <View>
-        <Text>Body Content</Text>
-    </View>
+  headerTitle="Station 3"
+  headerSubtitle="High Humidity Alarm"
+  headerInfo="4 Devices"
+  badge={
+    <HeroBanner
+      style={{
+        flex: 0,
+        flexBasis: 'auto',
+        minWidth: 140,
+        justifyContent: 'space-between',
+      }}>
+      <Hero
+        icon={{family: 'brightlayer-ui', name: 'temp'}}
+        label="Temperature"
+        ChannelValueProps={{value: 98, units: '°F'}}
+        style={{overflow: 'visible'}}
+        iconBackgroundColor={theme.colors.surface}
+      />
+      <Hero
+        icon={{family: 'brightlayer-ui', name: 'moisture'}}
+        label="Humidity"
+        ChannelValueProps={{value: 54, units: '%'}}
+        iconBackgroundColor={theme.colors.surface}
+      />
+    </HeroBanner>
+  }>
+  <View>
+    <Text>Body Content</Text>
+  </View>
 </ScoreCard>`;
 
 export const ScoreCardWithBadge = (): JSX.Element => (
   <Box>
     <ScoreCardWithBadgeExample />
-    <CodeBlock code={codeSnippet} language="jsx" dataLine="5-19" />
+    <CodeBlock code={codeSnippet} language="jsx" dataLine="5-27" />
     <CodeBlockActionButtonRow
       copyText={codeSnippet}
       url="componentDocs/Header/examples/ScoreCardWithBadgeExample.tsx"
