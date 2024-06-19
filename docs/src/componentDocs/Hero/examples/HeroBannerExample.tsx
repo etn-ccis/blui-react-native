9d3a2f43ae498f58a3df864ea6cc5cea07608467
priyankakmEaton <priyankakm@eaton.com>
2024-06-19 12:43:44 +0530

    Add multiple Hero

--- docs/src/componentDocs/Hero/examples/HeroBannerExample.tsx
+++ docs/src/componentDocs/Hero/examples/HeroBannerExample.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import {Hero, HeroBanner} from '@brightlayer-ui/react-native-components';
 import {ExampleShowcase} from '../../../shared';
+import {View} from 'react-native';
 
 export const HeroBannerExample = (): JSX.Element => (
   <ExampleShowcase>
-    <HeroBanner>
-      <Hero
-        icon={{family: 'brightlayer-ui', name: 'grade_a'}}
-        label="Efficiency"
-        ChannelValueProps={{value: '98', units: '%'}}
-      />
-    </HeroBanner>
+    <View style={{width: '70%', margin: 'auto'}}>
+      <HeroBanner>
+        <Hero
+          icon={{family: 'brightlayer-ui', name: 'grade_a'}}
+          label="Efficiency"
+          ChannelValueProps={{value: '98', units: '%'}}
+        />
+        <Hero
+          icon={{name: 'schedule'}}
+          label="Schedule"
+          ChannelValueProps={{value: '1', units: 'h'}}
+        />
+      </HeroBanner>
+    </View>
   </ExampleShowcase>
 );
