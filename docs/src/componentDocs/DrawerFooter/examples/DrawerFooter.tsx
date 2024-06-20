d95b62e60fd9932197830bbb236128ab718eee48
shubham-eaton <ShubhamKVishwakarma@eaton.com>
2024-06-20 19:10:06 +0530

    Added DrawerFooter content and styling changes

--- docs/src/componentDocs/DrawerFooter/examples/DrawerFooter.tsx
+++ docs/src/componentDocs/DrawerFooter/examples/DrawerFooter.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
 import {DrawerFooterExample} from './DrawerFooterExample';
 
-const codeSnippet = `<Drawer style={{width: 250, margin: 'auto'}}>
+const codeSnippet = `<Drawer style={{width: 320, margin: 'auto'}}>
       <DrawerBody>
         <DrawerNavItem
           itemID={'item1'}
@@ -22,16 +22,50 @@ const codeSnippet = `<Drawer style={{width: 250, margin: 'auto'}}>
         />
       </DrawerBody>
       <DrawerFooter>
-        <Text variant="displayMedium" style={{alignSelf: 'center'}}>
-          Footer content goes here
-        </Text>
+        <View style={{padding: 16}}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginBottom: 12,
+            }}>
+            <Text
+              style={{
+                fontSize: 12,
+                color: '#44474E',
+              }}>
+              v2.4.2
+            </Text>
+            <Text style={{fontSize: 12, color: '#44474E'}}>
+              10:33:05 05/12/2024
+            </Text>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Image
+              source={{uri: eatonLogo}}
+              style={{height: 32, width: '36%'}}
+              resizeMode="contain"
+            />
+            <Image
+              source={{uri: eatonCopyrightText}}
+              style={{height: 32, width: '33%'}}
+              resizeMode="contain"
+            />
+          </View>
+        </View>
       </DrawerFooter>
     </Drawer>`;
 
 export const DrawerFooter = (): JSX.Element => (
   <Box>
     <DrawerFooterExample />
-    <CodeBlock code={codeSnippet} language="jsx" dataLine='19-23' />
+    <CodeBlock code={codeSnippet} language="jsx" dataLine='19-57' />
     <CodeBlockActionButtonRow
       copyText={codeSnippet}
       url="componentDocs/DrawerFooter/examples/DrawerFooterExample.tsx"
--- docs/src/componentDocs/DrawerFooter/examples/DrawerFooterExample.tsx
+++ docs/src/componentDocs/DrawerFooter/examples/DrawerFooterExample.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Drawer, DrawerBody, DrawerFooter, DrawerNavItem} from '@brightlayer-ui/react-native-components';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerNavItem,
+} from '@brightlayer-ui/react-native-components';
 import {ExampleShowcase} from '../../../shared';
 import {Text} from 'react-native-paper';
+import {Image, View} from 'react-native';
+import eatonLogo from '../images/eatonLogo.png';
+import eatonCopyrightText from '../images/EatonCopyrightText.png';
 
 export const DrawerFooterExample = (): JSX.Element => (
   <ExampleShowcase>
-    <Drawer style={{width: 250, margin: 'auto'}}>
+    <Drawer style={{width: 320, margin: 'auto'}}>
       <DrawerBody>
         <DrawerNavItem
           itemID={'item1'}
@@ -24,9 +32,43 @@ export const DrawerFooterExample = (): JSX.Element => (
         />
       </DrawerBody>
       <DrawerFooter>
-        <Text variant="displayMedium" style={{alignSelf: 'center'}}>
-          Footer content goes here
-        </Text>
+        <View style={{padding: 16}}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginBottom: 12,
+            }}>
+            <Text
+              style={{
+                fontSize: 12,
+                color: '#44474E',
+              }}>
+              v2.4.2
+            </Text>
+            <Text style={{fontSize: 12, color: '#44474E'}}>
+              10:33:05 05/12/2024
+            </Text>
+          </View>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Image
+              source={{uri: eatonLogo}}
+              style={{height: 32, width: '36%'}}
+              resizeMode="contain"
+            />
+            <Image
+              source={{uri: eatonCopyrightText}}
+              style={{height: 32, width: '33%'}}
+              resizeMode="contain"
+            />
+          </View>
+        </View>
       </DrawerFooter>
     </Drawer>
   </ExampleShowcase>
