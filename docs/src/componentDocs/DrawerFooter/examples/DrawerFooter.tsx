185c1fcfb2a529ea284fc44575ea46607400f8da
shubham-eaton <ShubhamKVishwakarma@eaton.com>
2024-06-21 13:29:43 +0530

    Image changes for dark mode

--- docs/src/componentDocs/DrawerFooter/examples/DrawerFooter.tsx
+++ docs/src/componentDocs/DrawerFooter/examples/DrawerFooter.tsx
@@ -4,68 +4,75 @@ import {CodeBlock, CodeBlockActionButtonRow} from '../../../shared';
 import {DrawerFooterExample} from './DrawerFooterExample';
 
 const codeSnippet = `<Drawer style={{width: 320, margin: 'auto'}}>
-      <DrawerBody>
-        <DrawerNavItem
-          itemID={'item1'}
-          title={'Account'}
-          icon={{
-            family: 'material-community',
-            name: 'account',
-            direction: 'auto',
-          }}
-        />
-        <DrawerNavItem
-          itemID={'item2'}
-          title={'Notification'}
-          icon={{family: 'material-community', name: 'bell', direction: 'auto'}}
-          activeItemBackgroundShape={'round'}
-        />
-      </DrawerBody>
-      <DrawerFooter>
-        <View style={{padding: 16}}>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginBottom: 12,
-            }}>
-            <Text
+        <DrawerBody>
+          <DrawerNavItem
+            itemID={'account'}
+            title={'Account'}
+            icon={{
+              family: 'material-community',
+              name: 'account',
+              direction: 'auto',
+            }}
+          />
+          <DrawerNavItem
+            itemID={'notification'}
+            title={'Notification'}
+            icon={{
+              family: 'material-community',
+              name: 'bell',
+              direction: 'auto',
+            }}
+            activeItemBackgroundShape={'round'}
+          />
+        </DrawerBody>
+        <DrawerFooter>
+          <View style={{padding: 16}}>
+            <View
               style={{
-                fontSize: 12,
-                color: '#44474E',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginBottom: 12,
               }}>
-              v2.4.2
-            </Text>
-            <Text style={{fontSize: 12, color: '#44474E'}}>
-              10:33:05 05/12/2024
-            </Text>
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: theme.colors.onSurface,
+                }}>
+                v2.4.2
+              </Text>
+              <Text style={{fontSize: 12, color: theme.colors.onSurface}}>
+                10:33:05 05/12/2024
+              </Text>
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}>
+              <Image
+                source={{uri: EatonLogoLight}}
+                style={{height: 32, width: '36%'}}
+                resizeMode="contain"
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: theme.colors.onSurface,
+                  width: '39%',
+                }}>
+                {'Copyright © Eaton All Rights Reserved'}
+              </Text>
+            </View>
           </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-            <Image
-              source={{uri: eatonLogo}}
-              style={{height: 32, width: '36%'}}
-              resizeMode="contain"
-            />
-            <Image
-              source={{uri: eatonCopyrightText}}
-              style={{height: 32, width: '33%'}}
-              resizeMode="contain"
-            />
-          </View>
-        </View>
-      </DrawerFooter>
-    </Drawer>`;
+        </DrawerFooter>
+      </Drawer>`;
 
 export const DrawerFooter = (): JSX.Element => (
   <Box>
     <DrawerFooterExample />
-    <CodeBlock code={codeSnippet} language="jsx" dataLine='19-57' />
+    <CodeBlock code={codeSnippet} language="jsx" dataLine='23-64' />
     <CodeBlockActionButtonRow
       copyText={codeSnippet}
       url="componentDocs/DrawerFooter/examples/DrawerFooterExample.tsx"
--- docs/src/componentDocs/DrawerFooter/examples/DrawerFooterExample.tsx
+++ docs/src/componentDocs/DrawerFooter/examples/DrawerFooterExample.tsx
@@ -6,73 +6,80 @@ import {
   DrawerNavItem,
 } from '@brightlayer-ui/react-native-components';
 import {ExampleShowcase} from '../../../shared';
-import {Text, useTheme} from 'react-native-paper';
+import {Text} from 'react-native-paper';
 import {Image, View} from 'react-native';
-import eatonLogo from '../images/eatonLogo.png';
-import eatonLogoWhite from '../images/eatonLogoWhite.png';
-import { useExtendedTheme } from '@brightlayer-ui/react-native-themes';
+import EatonLogoLight from '../images/EatonLogoLight.png';
+import {useExtendedTheme} from '@brightlayer-ui/react-native-themes';
 
 export const DrawerFooterExample = (): JSX.Element => {
   const theme = useExtendedTheme();
-  const reactNativeTheme = useTheme();
   return (
-  <ExampleShowcase>
-    <Drawer style={{width: 320, margin: 'auto'}}>
-      <DrawerBody>
-        <DrawerNavItem
-          itemID={'account'}
-          title={'Account'}
-          icon={{
-            family: 'material-community',
-            name: 'account',
-            direction: 'auto',
-          }}
-        />
-        <DrawerNavItem
-          itemID={'notification'}
-          title={'Notification'}
-          icon={{family: 'material-community', name: 'bell', direction: 'auto'}}
-          activeItemBackgroundShape={'round'}
-        />
-      </DrawerBody>
-      <DrawerFooter>
-        <View style={{padding: 16}}>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginBottom: 12,
-            }}>
-            <Text
+    <ExampleShowcase>
+      <Drawer style={{width: 320, margin: 'auto'}}>
+        <DrawerBody>
+          <DrawerNavItem
+            itemID={'account'}
+            title={'Account'}
+            icon={{
+              family: 'material-community',
+              name: 'account',
+              direction: 'auto',
+            }}
+          />
+          <DrawerNavItem
+            itemID={'notification'}
+            title={'Notification'}
+            icon={{
+              family: 'material-community',
+              name: 'bell',
+              direction: 'auto',
+            }}
+            activeItemBackgroundShape={'round'}
+          />
+        </DrawerBody>
+        <DrawerFooter>
+          <View style={{padding: 16}}>
+            <View
               style={{
-                fontSize: 12,
-                color:theme.colors.onSurface,
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginBottom: 12,
               }}>
-              v2.4.2
-            </Text>
-            <Text style={{fontSize: 12, color: theme.colors.onSurface}}>
-              10:33:05 05/12/2024
-            </Text>
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: theme.colors.onSurface,
+                }}>
+                v2.4.2
+              </Text>
+              <Text style={{fontSize: 12, color: theme.colors.onSurface}}>
+                10:33:05 05/12/2024
+              </Text>
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}>
+              <Image
+                source={{uri: EatonLogoLight}}
+                style={{height: 32, width: '36%'}}
+                resizeMode="contain"
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: theme.colors.onSurface,
+                  width: '39%',
+                }}>
+                {'Copyright © Eaton All Rights Reserved'}
+              </Text>
+            </View>
           </View>
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-            <Image
-              source={{uri: reactNativeTheme.dark ? eatonLogoWhite : eatonLogo}}
-              style={{height: 32, width: '36%'}}
-              resizeMode="contain"
-            />
-            <Text style={{fontSize: 12, color: theme.colors.onSurface}}>
-            {'Copyright © Eaton\nAll Rights Reserved'}
-            </Text>
-          </View>
-        </View>
-      </DrawerFooter>
-    </Drawer>
-  </ExampleShowcase>
-);
-}
+        </DrawerFooter>
+      </Drawer>
+    </ExampleShowcase>
+  );
+};
