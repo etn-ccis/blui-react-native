1db04963dd975179aff44e71dbfbc3e017bbc1a3
manojleaton <manojlokesh@eaton.com>
2024-06-21 16:29:26 +0530

    added children to the collapsible header

--- docs/src/componentDocs/CollapsibleHeaderLayout/examples/CollapsibleHeaderLayoutExample.tsx
+++ docs/src/componentDocs/CollapsibleHeaderLayout/examples/CollapsibleHeaderLayoutExample.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {ExampleShowcase} from '../../../shared';
 import {CollapsibleHeaderLayout} from '@brightlayer-ui/react-native-components';
 import image from '../images/farm.jpg';
+import {getBodyFiller} from '../../../utils';
+import {View} from 'react-native';
 
 export const CollapsibleHeaderLayoutExample = (): JSX.Element => (
   <ExampleShowcase>
@@ -17,14 +19,15 @@ export const CollapsibleHeaderLayoutExample = (): JSX.Element => (
             onPress: () => {},
           },
         ],
-        variant: 'static',
+        variant: 'dynamic',
         backgroundImage: {uri: image},
         searchableConfig: {onChangeText: () => {}},
         expandable: true,
         collapsedHeight: 56,
         expandedHeight: 200,
         styles: {backgroundImage: {width: '100%'}},
-      }}
-    />
+      }}>
+      <View style={{height: 200}}>{getBodyFiller()}</View>
+    </CollapsibleHeaderLayout>
   </ExampleShowcase>
 );
