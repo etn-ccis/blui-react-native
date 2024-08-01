87d075000ebb8be3004d1843cc377c7746af892c
manojleaton <manojlokesh@eaton.com>
2024-08-01 17:28:30 +0530

    optional props

--- docs/src/componentDocs/MobileStepper/playground/PlaygroundPage.tsx
+++ docs/src/componentDocs/MobileStepper/playground/PlaygroundPage.tsx
@@ -1,59 +1,137 @@
 import React from 'react';
 import { Box, Stack } from '@mui/material';
-import { CodeSnippetFunction, InputConfig, Playground, PreviewComponent } from '@brightlayer-ui/react-doc-components';
+import {
+    CodeSnippetFunction,
+    getPropsMapping,
+    getPropsToString,
+    InputConfig,
+    Playground,
+    PreviewComponent,
+} from '@brightlayer-ui/react-doc-components';
 import { MobileStepper, MobileStepperProps } from '@brightlayer-ui/react-native-components';
 import { Button } from 'react-native-paper';
 
 const inputConfig: InputConfig = [
     // Required props
     {
-        id: 'activeStep',
-        type: 'number',
-        typeLabel: `number`,
-        initialValue: 1,
-        description: 'The index of the active step (>= 0)',
-        required: true,
-        category: 'Required Props',
-    },
-    {
         id: 'steps',
         type: 'number',
         typeLabel: `number`,
         initialValue: 1,
+        minValue: 1,
+        maxValue: 10,
+        valueStep: 1,
         description: 'Total number of steps to display (>0)',
         required: true,
         category: 'Required Props',
+        defaultValue: 1,
+    },
+    {
+        id: 'activeStep',
+        type: 'number',
+        typeLabel: `number`,
+        initialValue: 0,
+        description: 'The index of the active step (>= 0)',
+        minValue: 0,
+        maxValue: 9,
+        valueStep: 1,
+        required: true,
+        category: 'Required Props',
+        defaultValue: 0,
+    },
+    // Optional Props
+    {
+        id: 'activeColor',
+        type: 'color',
+        typeLabel: `string`,
+        initialValue: '',
+        description: 'Color of the active page indicator',
+        required: false,
+        category: 'Optional Props',
+    },
+    {
+        id: 'inactiveColor',
+        type: 'color',
+        typeLabel: `string`,
+        initialValue: '',
+        description: "Color of inactive dot when using 'dot' variant",
+        required: false,
+        category: 'Optional Props',
+    },
+    {
+        id: 'leftButton',
+        type: 'boolean',
+        typeLabel: `JSX.Element`,
+        label: 'leftButton',
+        description: 'Left button content',
+        required: false,
+        initialValue: true,
+        category: 'Optional Props',
+    },
+    {
+        id: 'rightButton',
+        type: 'boolean',
+        typeLabel: `JSX.Element`,
+        label: 'rightButton',
+        description: 'Right button content',
+        required: false,
+        initialValue: true,
+        category: 'Optional Props',
+    },
+    {
+        id: 'variant',
+        type: 'select',
+        typeLabel: 'string',
+        description: 'Which type of indicator to use',
+        initialValue: 'dots',
+        options: [
+            { label: 'dots', value: 'dots' },
+            { label: 'text', value: 'text' },
+            { label: 'progress', value: 'progress' },
+        ],
+        required: false,
+        category: 'Optional Props',
     },
 ];
 
-const generateSnippet: CodeSnippetFunction = (data) => ``;
+const generateSnippet: CodeSnippetFunction = (data) =>
+    `<MobileStepper
+    ${getPropsToString(getPropsMapping(data, inputConfig), { join: '\n\t' })}
+ />`
+        .replace(/^\s*$(?:\r\n?|\n)/gm, '')
+        .replace(/(?:^|)( {4}|\t)/gm, '    ');
 
 const MobileStepperPreview: PreviewComponent = ({ data }) => {
-    const { activeStep, steps, ...rest } = data as unknown as MobileStepperProps;
+    const { leftButton, rightButton, ...rest } = data as unknown as MobileStepperProps & {
+        leftButton?: boolean;
+        rightButton?: boolean;
+    };
 
     return (
         <Stack alignItems={'center'} justifyContent={'center'} sx={{ width: '100%', height: '100%' }}>
             <MobileStepper
                 {...rest}
-                activeStep={0}
-                steps={0}
                 leftButton={
-                    <Button
-                        onPress={() => ({})}
-                        mode="outlined"
-                        style={{ width: 100, alignSelf: 'flex-start', marginRight: 8 }}
-                    >
-                        Back
-                    </Button>
+                    leftButton ? (
+                        <Button
+                            onPress={() => ({})}
+                            mode="outlined"
+                            style={{ width: 100, alignSelf: 'flex-start', marginRight: 40 }}
+                        >
+                            Back
+                        </Button>
+                    ) : undefined
                 }
                 rightButton={
-                    <Button
-                        onPress={() => ({})}
-                        mode="contained"
-                        style={{ width: 100, alignSelf: 'flex-end', marginLeft: 8 }}
-                    >
-                        Next
-                    </Button>
+                    rightButton ? (
+                        <Button
+                            onPress={() => ({})}
+                            mode="contained"
+                            style={{ width: 100, alignSelf: 'flex-end', marginLeft: 40 }}
+                        >
+                            Next
+                        </Button>
+                    ) : undefined
                 }
             />
         </Stack>
