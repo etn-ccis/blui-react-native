143da0dda5a47ac64dca2590921be4cdc2de8a39
Komal Dhere <KomalGDhere@eaton.com>
2024-08-01 14:29:57 +0530

    Added playground for IconSwitch

--- docs/src/__configuration__/navigationMenu.tsx
+++ docs/src/__configuration__/navigationMenu.tsx
@@ -65,6 +65,7 @@ import { ChipPlaygroundComponent } from '../componentDocs/Chip/playground/Playgr
 import { DrawerHeaderPlaygroundComponent } from '../componentDocs/DrawerHeader/playground/PlaygroundPage';
 import { ScoreCardPlaygroundComponent } from '../componentDocs/ScoreCard/playground';
 import { ListItemTagPlaygroundComponent } from '../componentDocs/ListItemTag/playground';
+import { IconSwitchPlaygroundComponent } from '../componentDocs/IconSwitch/playground';
 
 export type RouteConfig = Omit<RouteProps, 'children'> & {
     title: string;
@@ -401,6 +402,11 @@ export const pageDefinitions: RouteConfig[] = [
                         title: 'API Docs',
                         path: 'api-docs',
                         element: <IconSwitchAPIDocs />,
+                    },
+                    {
+                        title: 'playground',
+                        path: 'playground',
+                        element: <IconSwitchPlaygroundComponent />,
                     },
                 ],
             },
