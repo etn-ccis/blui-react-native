acc75aa87f34c7fd10c922fba524befa537b5460
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2024-12-11 19:39:18 +0530

    Migrated docs to use MUI 6

--- docs/src/components/layout/SharedAppBar.tsx
+++ docs/src/components/layout/SharedAppBar.tsx
@@ -22,7 +22,7 @@ import Box from '@mui/material/Box';
 import { toggleDrawer, changeSiteTheme } from '../../redux/appState';
 // import { changeDirection } from '../redux/appState';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { useTheme } from '@mui/material/styles';
+import { useColorScheme, useTheme } from '@mui/material/styles';
 import { RootState } from '../../redux/store';
 import { SiteThemeType } from '../../__types__';
 // import { UIDirection } from '../__types__';
@@ -64,8 +64,11 @@ export const SharedAppBar: React.FC<SharedAppBarProps> = (props): JSX.Element =>
     // const siteDirection = useAppSelector((state: RootState) => state.appState.siteDirection);
     const linkToThemesOverview = `${import.meta.env.BASE_URL ? import.meta.env.BASE_URL : ''}/themes/overview`;
 
+    const { setMode } = useColorScheme();
+
     const onClickThemeSelectorItem = React.useCallback(
         (option: SiteThemeType): void => {
+            setMode(option);
             dispatch(
                 changeSiteTheme({
                     siteTheme: option,
--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -10,9 +10,9 @@ import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createTheme, ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import * as BLUIThemes from '@brightlayer-ui/react-themes';
+import { blueThemes } from '@brightlayer-ui/react-themes';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import '@brightlayer-ui/react-themes/open-sans';
 import '@fontsource/roboto';
@@ -52,17 +52,13 @@ const ThemedApp = (): JSX.Element => {
     const siteTheme = useAppSelector((state: RootState) => state.appState.siteTheme);
     const siteDirection = useAppSelector((state: RootState) => state.appState.siteDirection);
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-    let theme = BLUIThemes.blue;
-    if (siteTheme === 'dark' || (siteTheme === 'system' && prefersDarkMode)) {
-        theme = BLUIThemes.blueDark;
-    }
-    theme.direction = siteDirection;
+
     document.dir = siteDirection;
 
     // force an update
     const MemoThemedApp = React.useCallback(
         () => (
-            <ThemeProvider theme={createTheme(theme)}>
+            <ThemeProvider theme={{ ...blueThemes, direction: siteDirection }} defaultMode="light">
                 <RNThemeProvider
                     theme={
                         siteTheme === 'dark' || (siteTheme === 'system' && prefersDarkMode)
