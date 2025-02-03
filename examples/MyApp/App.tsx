d4fe6c47cb2b939b972e22e1cb15297e66d372a9
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2025-02-03 21:51:01 +0530

    Base setup for RN monorepo BLUI-6515

---
+++ examples/MyApp/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View,
+} from 'react-native';
+import {myVar} from 'test';
+function App(): React.JSX.Element {
+
+  return (
+    <View>
+     <Text> {myVar}</Text>
+    </View>
+  );
+}
+
+
+export default App;
