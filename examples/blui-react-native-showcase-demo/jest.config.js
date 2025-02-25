4747d55b8f3c8c13a40cc952a68eb4ff57ffac6a
manojleaton <manojlokesh@eaton.com>
2025-02-25 19:05:47 +0530

    added new showcase project

--- examples/blui-react-native-showcase-demo/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'react-native',
-    coverageDirectory: './coverage/',
-    collectCoverage: true,
-    transformIgnorePatterns: [
-        'node_modules/(?!(|react-native|@react-native/*|@brightlayer-ui/react-native-components|react-native-vector-icons|react-native-animatable|react-native-reanimated|react-native-iphone-x-helper|react-native-modal|react-native-collapsible|@react-native/polyfills|react-native-status-bar-height|react-native-webview|react-native-webview/*)/)',
-    ],
-};
