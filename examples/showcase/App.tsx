75c6ab19ee3d350aeeb86d2e79d7484ae26a72bd
manojleaton <manojlokesh@eaton.com>
2025-02-26 08:48:41 +0530

    added button on main screen

--- examples/showcase/App.tsx
+++ examples/showcase/App.tsx
@@ -6,128 +6,21 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button} from 'react-native-paper';
+import {useExtendedTheme} from '@brightlayer-ui/react-native-themes';
+import {Text, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-// import {
-//   Chip,
-//   InfoListItem,
-//   Overline,
-// } from '@brightlayer-ui/react-native-components/core';
-import {TextInput} from 'react-native-paper';
+function App(): React.JSX.Element {
+  const theme = useExtendedTheme();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>React Native Monorepo with Yarn workspaces</Text>
+      <Button mode="contained" style={{backgroundColor: theme.colors.primary}}>
+        Brightlayer UI
+      </Button>
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      {/* <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView> */}
-      {/* <Overline>Test</Overline> */}
-      {/* <Chip>Test</Chip> */}
-      <TextInput label="Email" />
-      {/* <InfoListItem
-        title={'Title'}
-        subtitle={'Subtitle'}
-        info={'Info'}
-        hidePadding
-      /> */}
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- examples/showcase/index.js
+++ examples/showcase/index.js
@@ -5,13 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {PaperProvider} from 'react-native-paper';
+import {Provider as ThemeProvider} from 'react-native-paper';
+import * as BLUIThemes from '@brightlayer-ui/react-native-themes';
 
 export default function Main() {
   return (
-    <PaperProvider>
+    <ThemeProvider theme={BLUIThemes.blueDark}>
       <App />
-    </PaperProvider>
+    </ThemeProvider>
   );
 }
 
