1efea6113cc4c8f90e6ea1863cdb17d90d415059
manojleaton <manojlokesh@eaton.com>
2025-03-20 18:48:10 +0530

    packages version updates

--- packages/component-library/babel.config.js
+++ packages/component-library/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset', '@babel/preset-react', '@babel/preset-typescript'],
+    presets: ['module:@react-native/babel-preset', '@babel/preset-react', '@babel/preset-typescript'],
     // react-native-reanimated/plugin must be listed last
     plugins: [
         'react-native-reanimated/plugin',
