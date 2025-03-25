115721655eea4f327216e1358deaff2724c1d5c1
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2025-03-25 12:09:47 -0400

    Add code-cov config generate report and test upload

--- packages/component-library/babel.config.js
+++ packages/component-library/babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset', '@babel/preset-react', '@babel/preset-typescript'],
+    presets: ['module:metro-react-native-babel-preset', 'module:@react-native/babel-preset'],
     // react-native-reanimated/plugin must be listed last
-    plugins: [
-        'react-native-reanimated/plugin',
-        'module:react-native-dotenv',
-        '@babel/plugin-transform-modules-commonjs',
-        ['@babel/plugin-transform-private-methods', { loose: true }],
-        ['@babel/plugin-transform-class-properties', { loose: true }],
-        ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ],
+    plugins: ['react-native-reanimated/plugin'],
 };
