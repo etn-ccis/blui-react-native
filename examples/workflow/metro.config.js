e613e0906af5220bcb46b614f45c7fab24ccec9e
Arshdeep Singh <arshdeepsingh3@eaton.com>
2025-05-06 12:29:59 +0530

    updated example metro config

--- examples/workflow/metro.config.js
+++ examples/workflow/metro.config.js
@@ -1,17 +1,13 @@
 const { getDefaultConfig } = require('@react-native/metro-config');
 const path = require('path');
 
-const workspaceRoot = path.resolve(__dirname, '../../');
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [workspaceRoot];
+config.watchFolders = [require('node:path').resolve(__dirname, '..')];
 
-config.resolver.nodeModulesPaths = [
-    path.resolve(projectRoot, 'node_modules'),
-    path.resolve(workspaceRoot, 'node_modules'),
-];
+config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules')];
 
 config.resolver.unstable_enableSymlinks = true;
 config.resolver.unstable_enablePackageExports = true;
