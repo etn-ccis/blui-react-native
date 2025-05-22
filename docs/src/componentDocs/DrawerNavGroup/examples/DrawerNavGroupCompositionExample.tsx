be2c6e29aca9c624fe6d90d9218b6f1230e1898a
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2025-05-22 11:12:29 +0530

    Added onSurace font color for drawer examples

--- docs/src/componentDocs/DrawerNavGroup/examples/DrawerNavGroupCompositionExample.tsx
+++ docs/src/componentDocs/DrawerNavGroup/examples/DrawerNavGroupCompositionExample.tsx
@@ -3,32 +3,37 @@ import { Drawer, DrawerBody, DrawerNavGroup, DrawerNavItem } from '@brightlayer-
 import { ExampleShowcase } from '../../../shared';
 import Divider from '@mui/material/Divider';
 import { DRAWER_WIDTH } from '../../../utils';
+import { useExtendedTheme } from '@brightlayer-ui/react-native-themes';
 
-export const DrawerNavGroupCompositionExample = (): JSX.Element => (
-    <ExampleShowcase>
-        <Drawer style={{ width: DRAWER_WIDTH, margin: 'auto' }}>
-            <DrawerBody>
-                <DrawerNavGroup title="Group 1" hidePadding titleDivider={false}>
-                    <DrawerNavItem title="Item 1" itemID="1" />
-                    <DrawerNavItem title="Item 2" itemID="2" />
-                </DrawerNavGroup>
-                <Divider />
-                <DrawerNavGroup
-                    title="Group 2"
-                    hidePadding
-                    titleDivider={false}
-                    items={[
-                        {
-                            title: 'Item 3',
-                            itemID: '3',
-                        },
-                        {
-                            title: 'Item 4',
-                            itemID: '4',
-                        },
-                    ]}
-                />
-            </DrawerBody>
-        </Drawer>
-    </ExampleShowcase>
-);
+export const DrawerNavGroupCompositionExample = (): JSX.Element => {
+    const theme = useExtendedTheme();
+    return (
+        <ExampleShowcase>
+            <Drawer style={{ width: DRAWER_WIDTH, margin: 'auto' }}>
+                <DrawerBody>
+                    <DrawerNavGroup title="Group 1" hidePadding titleDivider={false}>
+                        <DrawerNavItem itemFontColor={theme.colors.onSurface} title="Item 1" itemID="1" />
+                        <DrawerNavItem itemFontColor={theme.colors.onSurface} title="Item 2" itemID="2" />
+                    </DrawerNavGroup>
+                    <Divider />
+                    <DrawerNavGroup
+                        itemFontColor={theme.colors.onSurface}
+                        title="Group 2"
+                        hidePadding
+                        titleDivider={false}
+                        items={[
+                            {
+                                title: 'Item 3',
+                                itemID: '3',
+                            },
+                            {
+                                title: 'Item 4',
+                                itemID: '4',
+                            },
+                        ]}
+                    />
+                </DrawerBody>
+            </Drawer>
+        </ExampleShowcase>
+    );
+};
