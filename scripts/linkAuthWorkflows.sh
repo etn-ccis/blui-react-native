acf8d6355364fac458c2b139f7535d86a54484e3
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2025-06-20 13:59:09 +0530

    BUild link script changes

--- scripts/linkAuthWorkflows.sh
+++ scripts/linkAuthWorkflows.sh
@@ -14,33 +14,33 @@ NC='\033[0m' # No Color
 
 echo -en "${BLUE}Creating new folder in node_modules...${NC}"
 rm -rf "./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow"
-mkdir -p "./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/dist"
+mkdir -p "./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow"
 rm -rf "./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow"
-mkdir -p "./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/dist"
+mkdir -p "./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow"
 
 echo -e "${GREEN}Done${NC}"
 
 echo -en "${BLUE}Copying build output into node_modules...${NC}";
 
-cp ./packages/auth-workflows/package.json ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/package.json
-cp -r ./packages/auth-workflows/dist/ ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/dist
-cp ./packages/auth-workflows/package.json ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/package.json
-cp -r ./packages/auth-workflows/dist/ ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/dist
+# cp ./packages/auth-workflows/package.json ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/package.json
+cp -r ./packages/auth-workflows/dist/ ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/
+# cp ./packages/auth-workflows/package.json ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/package.json
+cp -r ./packages/auth-workflows/dist/ ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/
 echo -e "${GREEN}Done${NC}"
 
 echo -en "\r\n${BBLUE}Linking Components: ${NC}"
 if [ ! -f ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/package.json ]; then echo -e "${BRED}Auth Worklow Not Linked${NC}" && exit 1; fi
-if [ ! -s ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/dist ];
-    then
-        if [ ! -f ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/dist/index.js ];
-        then echo -e "${BRED}Auth Workflows Not Linked${NC}" && exit 1;
-        fi;
-fi
+# if [ ! -s ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/dist ];
+#     then
+#         if [ ! -f ./examples/showcase/node_modules/@brightlayer-ui/react-native-auth-workflow/dist/index.js ];
+#         then echo -e "${BRED}Auth Workflows Not Linked${NC}" && exit 1;
+#         fi;
+# fi
 if [ ! -f ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/package.json ]; then echo -e "${BRED}Auth Worklow Not Linked${NC}" && exit 1; fi
-if [ ! -s ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/dist ];
-    then
-        if [ ! -f ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/dist/index.js ];
-        then echo -e "${BRED}Auth Workflows Not Linked${NC}" && exit 1;
-        fi;
-fi
+# if [ ! -s ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/dist ];
+#     then
+#         if [ ! -f ./examples/workflow/node_modules/@brightlayer-ui/react-native-auth-workflow/dist/index.js ];
+#         then echo -e "${BRED}Auth Workflows Not Linked${NC}" && exit 1;
+#         fi;
+# fi
 echo -e "${GRAY}Complete${NC}\r\n"
