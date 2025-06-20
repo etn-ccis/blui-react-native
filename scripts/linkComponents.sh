8e0955b726e6de240977f295439c060e51fca95a
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2025-06-20 12:34:29 +0530

    Updated scripts

--- scripts/linkComponents.sh
+++ scripts/linkComponents.sh
@@ -14,11 +14,11 @@ NC='\033[0m' # No Color
 
 echo -en "${BLUE}Creating new folder in node_modules...${NC}"
 rm -rf "./examples/showcase/node_modules/@brightlayer-ui/react-native-components"
-mkdir -p "./examples/showcase/node_modules/@brightlayer-ui/react-native-components/dist"
+mkdir -p "./examples/showcase/node_modules/@brightlayer-ui/react-native-components"
 rm -rf "./examples/workflow/node_modules/@brightlayer-ui/react-native-components"
-mkdir -p "./examples/workflow/node_modules/@brightlayer-ui/react-native-components/dist"
+mkdir -p "./examples/workflow/node_modules/@brightlayer-ui/react-native-components"
 rm -rf "./examples/designPatterns/node_modules/@brightlayer-ui/react-native-components"
-mkdir -p "./examples/designPatterns/node_modules/@brightlayer-ui/react-native-components/dist"
+mkdir -p "./examples/designPatterns/node_modules/@brightlayer-ui/react-native-components"
 
 echo -e "${GREEN}Done${NC}"
 
@@ -34,24 +34,24 @@ echo -e "${GREEN}Done${NC}"
 
 echo -en "\r\n${BBLUE}Linking Components: ${NC}"
 if [ ! -f ./examples/showcase/node_modules/@brightlayer-ui/react-native-components/package.json ]; then echo -e "${BRED}Components Not Linked${NC}" && exit 1; fi
-if [ ! -s ./examples/showcase/node_modules/@brightlayer-ui/react-native-components/dist ];
-    then
-        if [ ! -f ./examples/showcase/node_modules/@brightlayer-ui/react-native-components/dist/index.js ];
-        then echo -e "${BRED}Component Not Linked to showcase${NC}" && exit 1;
-        fi;
-fi
+# if [ ! -s ./examples/showcase/node_modules/@brightlayer-ui/react-native-components/dist ];
+#     then
+#         if [ ! -f ./examples/showcase/node_modules/@brightlayer-ui/react-native-components/dist/index.js ];
+#         then echo -e "${BRED}Component Not Linked to showcase${NC}" && exit 1;
+#         fi;
+# fi
 if [ ! -f ./examples/workflow/node_modules/@brightlayer-ui/react-native-components/package.json ]; then echo -e "${BRED}Components Not Linked${NC}" && exit 1; fi
-if [ ! -s ./examples/workflow/node_modules/@brightlayer-ui/react-native-components/dist ];
-    then
-        if [ ! -f ./examples/workflow/node_modules/@brightlayer-ui/react-native-components/dist/index.js ];
-        then echo -e "${BRED}Components Not Linked to workflow${NC}" && exit 1;
-        fi;
-fi
+# if [ ! -s ./examples/workflow/node_modules/@brightlayer-ui/react-native-components/dist ];
+#     then
+#         if [ ! -f ./examples/workflow/node_modules/@brightlayer-ui/react-native-components/dist/index.js ];
+#         then echo -e "${BRED}Components Not Linked to workflow${NC}" && exit 1;
+#         fi;
+# fi
 if [ ! -f ./examples/designPatterns/node_modules/@brightlayer-ui/react-native-components/package.json ]; then echo -e "${BRED}Components Not Linked${NC}" && exit 1; fi
-if [ ! -s ./examples/designPatterns/node_modules/@brightlayer-ui/react-native-components/dist ];
-    then
-        if [ ! -f ./examples/designPatterns/node_modules/@brightlayer-ui/react-native-components/dist/index.js ];
-        then echo -e "${BRED}Components Not Linked to Design Patterns${NC}" && exit 1;
-        fi;
-fi
+# if [ ! -s ./examples/designPatterns/node_modules/@brightlayer-ui/react-native-components/dist ];
+#     then
+#         if [ ! -f ./examples/designPatterns/node_modules/@brightlayer-ui/react-native-components/dist/index.js ];
+#         then echo -e "${BRED}Components Not Linked to Design Patterns${NC}" && exit 1;
+#         fi;
+# fi
 echo -e "${GRAY}Complete${NC}\r\n"
