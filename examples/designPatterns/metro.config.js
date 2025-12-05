db9132ab4fd6e427df6ec6ae90a3cac9aae294ff
manojleaton <manojlokesh@eaton.com>
2025-12-05 16:03:32 +0530

    fix: create watchPackages symlinks for designPatterns example
    - Create watchPackages directory structure with symlinks to monorepo packages
    - Update metro.config.js and tsconfig.json configurations
    - Fix ENOENT error for metro file watcher

--- examples/designPatterns/metro.config.js
+++ examples/designPatterns/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,40 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
+
+const projectRoot = __dirname;
+
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+config.watchFolders = [
+  require('node:path').resolve(__dirname, '..'),
+  require('node:path').resolve(
+    __dirname,
+    './watchPackages/@brightlayer-ui/react-native-components',
+  ),
+];
+
+config.resolver = {
+  ...config.resolver,
+  alias: {
+    '@brightlayer-ui/react-native-components': path.resolve(
+      __dirname,
+      './watchPackages/@brightlayer-ui/react-native-components',
+    ),
+    '@brightlayer-ui/react-native-themes': path.resolve(
+      __dirname,
+      './watchPackages/@brightlayer-ui/react-native-themes',
+    ),
+  },
+  nodeModulesPaths: [
+    path.resolve(__dirname, './watchPackages'),
+    path.resolve(projectRoot, 'node_modules'),
+  ],
+};
+
+module.exports = wrapWithReanimatedMetroConfig(
+  mergeConfig(getDefaultConfig(__dirname), config),
+);
