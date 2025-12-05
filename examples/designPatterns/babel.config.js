4639185e494674a0d59edd13d69d466fb0069a3a
manojleaton <manojlokesh@eaton.com>
2025-12-05 11:16:04 +0530

    feat: upgrade design patterns example to React Native 0.76.5
    - Update React Native from 0.73.4 to 0.76.5
    - Update dependencies and configurations
    - Add ESLint configuration
    - Update Android build configuration and Gradle wrapper
    - Update iOS project files and Podfile
    - Remove package-lock.json in favor of yarn.lock
    - Update metro and babel configurations

--- examples/designPatterns/babel.config.js
+++ examples/designPatterns/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    // react-native-reanimated should be last in the plugins array
-    'react-native-reanimated/plugin',
-  ],
+  plugins: ['react-native-worklets/plugin'],
 };
--- examples/designPatterns/metro.config.js
+++ examples/designPatterns/metro.config.js
@@ -1,5 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const path = require('path');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -7,40 +6,6 @@ const path = require('path');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
-
-const projectRoot = __dirname;
-
 const config = {};
 
-config.watchFolders = [
-  require('node:path').resolve(__dirname, '..'),
-  require('node:path').resolve(
-    __dirname,
-    './watchPackages/@brightlayer-ui/react-native-components',
-  ),
-];
-
-config.resolver = {
-  ...config.resolver,
-  alias: {
-    '@brightlayer-ui/react-native-components': path.resolve(
-      __dirname,
-      './watchPackages/@brightlayer-ui/react-native-components',
-    ),
-    '@brightlayer-ui/react-native-themes': path.resolve(
-      __dirname,
-      './watchPackages/@brightlayer-ui/react-native-themes',
-    ),
-  },
-  nodeModulesPaths: [
-    path.resolve(__dirname, './watchPackages'),
-    path.resolve(projectRoot, 'node_modules'),
-  ],
-};
-
-module.exports = wrapWithReanimatedMetroConfig(
-  mergeConfig(getDefaultConfig(__dirname), config),
-);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
