518ef699fb894fe28e56bd04b8559d20f4abc6fa
manojleaton <manojlokesh@eaton.com>
2026-02-10 12:23:38 +0530

    routing template app name change

--- packages/cli-templates/routing-typescript/template/android/app/src/main/java/com/routingtemplate/MainActivity.kt
+++ packages/cli-templates/routing-typescript/template/android/app/src/main/java/com/routingtemplate/MainActivity.kt
@@ -17,7 +17,7 @@ override fun onCreate(savedInstanceState: Bundle?) {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "blankTemplate"
+  override fun getMainComponentName(): String = "RoutingTemplate"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
