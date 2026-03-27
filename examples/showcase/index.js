7abcc9a3cbb7f052058266f644c58d57e223a969
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2026-03-27 13:10:28 +0530

    Added automatic theme switching logic based on device's current theme

--- examples/showcase/index.js
+++ examples/showcase/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, useColorScheme } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { name as appName } from './app.json';
 import { blue, blueDark } from '@brightlayer-ui/react-native-themes';
@@ -8,7 +8,14 @@ import { ThemeContext } from './contexts/ThemeContext';
 import { MainRouter } from './router';
 
 const Wrapper = () => {
-  const [theme, setTheme] = useState('light');
+  const deviceColorScheme = useColorScheme();
+  const [theme, setTheme] = useState(
+    deviceColorScheme === 'dark' ? 'dark' : 'light',
+  );
+
+  useEffect(() => {
+    setTheme(deviceColorScheme === 'dark' ? 'dark' : 'light');
+  }, [deviceColorScheme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
