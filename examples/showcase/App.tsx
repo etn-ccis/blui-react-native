62cc8924dc9ea666e71bcf8632b4f6e94aa213ce
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2026-03-30 16:15:45 +0530

    Revert "Changed user menu options to use system default or light or dark mode"
    This reverts commit 3f9a4ff5049b57fb2352db8ca1dff1632b9434ee.

--- examples/showcase/App.tsx
+++ examples/showcase/App.tsx
@@ -11,6 +11,7 @@ import { I18nManager } from 'react-native';
 import { KitchenSink } from './components/KitchenSink';
 import { CollapsibleHeaderLayout } from '@brightlayer-ui/react-native-components';
 import { UserMenuExample } from './components/UserMenuExample';
+import { useThemeContext } from './contexts/ThemeContext';
 import RNRestart from 'react-native-restart';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from './router';
@@ -29,33 +30,44 @@ type AppProps = {
   navigation: StackNavigationProp<RootStackParamList, 'App'>;
 };
 
-export const App: React.FC<AppProps> = ({ navigation }) => (
-  <CollapsibleHeaderLayout
-    HeaderProps={{
-      variant: 'dynamic',
-      title: 'Valley Forge',
-      subtitle: 'The Last Stand',
-      icon: { name: 'menu' },
-      info: 'hello',
-      expandable: true,
-      backgroundImage: require('./assets/images/farm.jpg'),
-      onIconPress: (): void => {
-        navigation.openDrawer();
-      },
-      searchableConfig: { placeholder: 'Search', autoFocus: true },
-      actionItems: [
-        {
-          icon: { name: 'more' },
-          onPress: (): void => {},
-          component: <UserMenuExample onToggleRTL={toggleRTL} />,
+export const App: React.FC<AppProps> = ({ navigation }) => {
+  const { theme: themeType, setTheme } = useThemeContext();
+
+  return (
+    <CollapsibleHeaderLayout
+      HeaderProps={{
+        variant: 'dynamic',
+        title: 'Valley Forge',
+        subtitle: 'The Last Stand',
+        icon: { name: 'menu' },
+        info: 'hello',
+        expandable: true,
+        backgroundImage: require('./assets/images/farm.jpg'),
+        onIconPress: (): void => {
+          navigation.openDrawer();
         },
-      ],
-    }}
-    ScrollViewProps={{
-      nestedScrollEnabled: true,
-      keyboardShouldPersistTaps: 'handled',
-    }}
-  >
-    <KitchenSink />
-  </CollapsibleHeaderLayout>
-);
+        searchableConfig: { placeholder: 'Search', autoFocus: true },
+        actionItems: [
+          {
+            icon: { name: 'more' },
+            onPress: (): void => {},
+            component: (
+              <UserMenuExample
+                onToggleRTL={toggleRTL}
+                onToggleTheme={(): void =>
+                  setTheme(themeType === 'light' ? 'dark' : 'light')
+                }
+              />
+            ),
+          },
+        ],
+      }}
+      ScrollViewProps={{
+        nestedScrollEnabled: true,
+        keyboardShouldPersistTaps: 'handled',
+      }}
+    >
+      <KitchenSink />
+    </CollapsibleHeaderLayout>
+  );
+};
--- examples/showcase/components/UserMenuExample.tsx
+++ examples/showcase/components/UserMenuExample.tsx
@@ -1,5 +1,4 @@
 import {
-  IconSwitch,
   InfoListItemProps,
   UserMenu,
 } from '@brightlayer-ui/react-native-components';
@@ -7,26 +6,15 @@ import React from 'react';
 import { Avatar } from 'react-native-paper';
 import * as BLUIColors from '@brightlayer-ui/colors';
 import { IconFamily } from '@brightlayer-ui/react-native-components/md3/__types__';
-import { useThemeContext } from '../contexts/ThemeContext';
 
 const SwapIcon: IconFamily = {
   family: 'material',
   name: 'swap-horiz',
   direction: 'ltr',
 };
-const SystemDefaultIcon: IconFamily = {
+const InvertColorsIcon: IconFamily = {
   family: 'material',
-  name: 'settings-brightness',
-  direction: 'ltr',
-};
-const LightModeIcon: IconFamily = {
-  family: 'material',
-  name: 'light-mode',
-  direction: 'ltr',
-};
-const DarkModeIcon: IconFamily = {
-  family: 'material',
-  name: 'dark-mode',
+  name: 'invert-colors',
   direction: 'ltr',
 };
 const CancelIcon: IconFamily = {
@@ -37,55 +25,19 @@ const CancelIcon: IconFamily = {
 
 type UserMenuExampleProps = {
   onToggleRTL: () => void;
+  onToggleTheme: () => void;
 };
 
 export const UserMenuExample: React.FC<UserMenuExampleProps> = props => {
-  const { onToggleRTL } = props;
-  const { theme, setTheme, followSystem, setFollowSystem } = useThemeContext();
+  const { onToggleRTL, onToggleTheme } = props;
 
   const menuItems: InfoListItemProps[] = [
     { title: 'Toggle RTL', icon: SwapIcon, onPress: (): void => onToggleRTL() },
     {
-      title: 'Theme: System Default',
-      icon: SystemDefaultIcon,
-      onPress: (): void => setFollowSystem(!followSystem),
-      rightComponent: (
-        <IconSwitch
-          value={followSystem}
-          onValueChange={(): void => setFollowSystem(!followSystem)}
-          showIcon
-        />
-      ),
+      title: 'Toggle Theme',
+      icon: InvertColorsIcon,
+      onPress: (): void => onToggleTheme(),
     },
-    // Show Light/Dark options only when not following system
-    ...(!followSystem
-      ? [
-          {
-            title: 'Light Mode',
-            icon: LightModeIcon,
-            onPress: (): void => setTheme('light'),
-            rightComponent: (
-              <IconSwitch
-                value={theme === 'light'}
-                onValueChange={(): void => setTheme('light')}
-                showIcon
-              />
-            ),
-          },
-          {
-            title: 'Dark Mode',
-            icon: DarkModeIcon,
-            onPress: (): void => setTheme('dark'),
-            rightComponent: (
-              <IconSwitch
-                value={theme === 'dark'}
-                onValueChange={(): void => setTheme('dark')}
-                showIcon
-              />
-            ),
-          },
-        ]
-      : []),
     { title: 'Cancel', icon: CancelIcon },
   ];
 
