f0dd09bdedb45d4531d5b64d494dab4ab442a6ae
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2026-06-02 14:00:41 +0530

    Added MaterialDesginIcon ttf file and used it in empty state button icon.
    https://eaton-corp.atlassian.net/browse/BLUI-7320

--- docs/src/componentDocs/EmptyState/examples/EmptyStateWithActions.tsx
+++ docs/src/componentDocs/EmptyState/examples/EmptyStateWithActions.tsx
@@ -3,18 +3,24 @@ import Box from '@mui/material/Box';
 import { CodeBlock, CodeBlockActionButtonRow } from '../../../shared';
 import { EmptyStateWithActionsExample } from './EmptyStateWithActionsExample';
 
-const codeSnippet = `<EmptyState
+const codeSnippet = `
+<EmptyState
     icon={{name: 'devices'}}
     title={'No Devices'}
     description={'Check your network connection or add a new device'}
     actions={
-        <Button icon={'plus'} mode="outlined">
+        <Button
+            icon={({ size, color }) => (
+                <Icon source={{ family: 'material-community', name: 'plus' }} size={size} color={color} />
+            )}
+            mode="outlined"
+        >
             Add Device
         </Button>
     }
 />`;
 
-export const EmptyStateWithActions = (): JSX.Element => (
+export const EmptyStateWithActions = (): React.JSX.Element => (
     <Box>
         <EmptyStateWithActionsExample />
         <CodeBlock code={codeSnippet} language="jsx" dataLine={'5, 6-9'} />
--- docs/src/componentDocs/EmptyState/examples/EmptyStateWithActionsExample.tsx
+++ docs/src/componentDocs/EmptyState/examples/EmptyStateWithActionsExample.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Button } from 'react-native-paper';
-import { EmptyState } from '@brightlayer-ui/react-native-components';
+import { EmptyState, Icon } from '@brightlayer-ui/react-native-components';
 import { ExampleShowcase } from '../../../shared';
 import { DEMO_WIDTH } from '../../../utils';
 
-export const EmptyStateWithActionsExample = (): JSX.Element => (
+export const EmptyStateWithActionsExample = (): React.JSX.Element => (
     <ExampleShowcase>
         <EmptyState
             icon={{ name: 'devices' }}
             title={'No Devices'}
             description={'Check your network connection or add a new device'}
             actions={
-                <Button icon={'plus'} mode="outlined">
+                <Button
+                    icon={({ size, color }) => (
+                        <Icon source={{ family: 'material-community', name: 'plus' }} size={size} color={color} />
+                    )}
+                    mode="outlined"
+                >
                     Add Device
                 </Button>
             }
