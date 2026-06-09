19e5985bb1c42617208862d718b2860e0e7598d0
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2026-06-09 11:51:10 +0530

    added comma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ export default [
             'examples/showcase-expo/app/**',
             'examples/showcase-expo/.expo/**',
             'examples/**/watchPackages/**',
-        ]
+        ],
     },
     {
         files: ['**/*.{ts,tsx}'],
